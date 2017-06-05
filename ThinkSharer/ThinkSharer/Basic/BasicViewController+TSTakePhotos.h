300c3d7c0331693845df1897b59405cc7bee4c42
zhangdekai <zdkzhuizhu@163.com>
2017-06-05 22:27:13 +0800

    fix ui

---
+++ ThinkSharer/ThinkSharer/Basic/BasicViewController+TSTakePhotos.h
@@ -0,0 +1,21 @@
+//
+//  BasicViewController+TSTakePhotos.h
+//  ThinkSharer
+//
+//  Created by 张德凯 on 2017/6/5.
+//  Copyright © 2017年 张德凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BasicViewController (TSTakePhotos)
+
+
+/**
+ 选择相册，照片
+ */
+- (void)selectPhoto;
+
+
+@end
